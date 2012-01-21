59ae2f3eba0d9b0246e8d4ebb40fa2313780dae2
Aaron Steele <eightysteele@gmail.com>
2012-01-20 21:07:44 -0800

    Add org.clojure/data.json dep to project.clj.

--- project.clj
+++ project.clj
@@ -8,6 +8,7 @@
   :javac-options {:debug "true" :fork "true"}
   :jvm-opts ["-XX:MaxPermSize=128M" "-Xms1024M" "-Xmx2048M" "-server"]
   :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/data.json "0.1.1"]
                  [org.clojure/tools.cli "0.2.1"]
                  [clojure-csv/clojure-csv "1.3.2"]
                  [org.clojure/math.numeric-tower "0.0.1"]
