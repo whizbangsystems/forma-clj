9edaad8b070c228f0ce1c531aa936d48883644de
Dan Hammer <dan.s.hammer@gmail.com>
2012-03-31 17:14:57 -0700

    new documentation push.

--- test/forma/hadoop/io_test.clj
+++
@@ -1,9 +0,0 @@
-(ns forma.hadoop.io-test
-  (:use [forma.hadoop.io] :reload)
-  (:use midje.sweet))
-
-(tabular
- (fact "Globstring test."
-   (apply globstring ?args) => ?result)
- ?args                        ?result
- [* "hi" ["008006" "033011"]] "*/hi/{008006,033011}")
--- test/forma/static_test.clj
+++
@@ -1,7 +0,0 @@
-(ns forma.static-test
-  (:use forma.static
-        midje.sweet))
-
-(fact "static-datasets field test."
-  (for [[_ v] static-datasets]
-    (keys v) => (just [:corner :travel :step :nodata] :in-any-order)))
